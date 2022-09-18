0e97874c265d74306c5a15d106960f77638fafd5
Antor <antor@gmail.com>
2022-09-18 22:03:25 +0600

    first commit progrom

---
+++ script.js
@@ -0,0 +1,10 @@
+var navLinks = document.getelementById("navLinks");
+      function showMenu() {
+        
+        navLinks.style.right = "0";  
+        console.log(navLinks)
+      }
+      
+      function hideMenu() {
+        navLinks.style.right = "-200px";
+      }
